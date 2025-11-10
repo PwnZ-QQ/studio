02772402352d9f17ba673860381d2af1bfb87340
František Kalášek <topbot@topwnz.com>
2025-11-10 22:53:53 +0000

    ano super

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -5,3 +5,4 @@ import '@/ai/flows/real-time-text-translation.ts';
 import '@/ai/flows/object-identification.ts';
 import '@/ai/flows/describe-object-flow.ts';
 import '@/ai/flows/get-ar-object-details.ts';
+import '@/ai/flows/text-to-speech.ts';
